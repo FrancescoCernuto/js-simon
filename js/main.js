89575a66c737b6e5b69e08e19e6e7f3ddf5250ec
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-10-25 01:00:50 +0200

    add-alldoms

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// # Selecting DOM Elements
+const numberOneEl = document.querySelector("#numberOne");
+const numberTwoEl = document.querySelector("#numberTwo");
+const numberThreeEl = document.querySelector("#numberThree");
+const numberFourEl = document.querySelector("#numberFour");
+const numberFiveEl = document.querySelector("#numberFive");
+const startGameEl = document.querySelector("#startGame");
+const submitChoicesEl = document.querySelector("#submitChoices");
+const displayResultEl = document.querySelector("#displayResult");
+const correctAnswersEl = document.querySelector("#correctAnswers");
